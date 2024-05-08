e239aaa7bd781581e03cb3c0bf508a0ae0142a77
Nawgire Swapnil Mahadev <nawgire.swapnil@m2pfintech.com>
2024-05-08 23:19:14 +0530

    new changes added

--- Recurssion/Subsequence/subsequence.java
+++ Recurssion/Subsequence/subsequence.java
@@ -1,10 +1,38 @@
 package Recurssion.Subsequence;
 
+import java.util.ArrayList;
+
 public class subsequence {
 
-    public static int
+    public static void printSubsequence(ArrayList<Integer> list, int i, int n,int [] arr)
+    {
+        if(i>=n)
+        {
+            System.out.println(list);
+            return;
+        }
+
+
+
+
+
+        list.add(arr[i]);
+        printSubsequence(list,i+1,n,arr);
+       list.removeLast();
+        printSubsequence(list,i+1,n,arr);
+
+
+
+    }
+
 
     public static void main(String[] args) {
+
+        int [] arr1={3,1,2};
+
+        ArrayList<Integer> list1 =new ArrayList<>();
+
+        printSubsequence(list1,0,arr1.length,arr1);
 
     }
 
